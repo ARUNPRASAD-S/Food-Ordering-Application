64fed0205d6eb3f58694094bc42a5285dd997008
Arun Prasad S <sarunprasad@ssdi.sharp.co.in>
2023-11-20 16:09:39 +0530

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadersComponent } from './components/partial/headers/headers.component';
-
+import { HomeComponent } from './components/pages/home/home.component';
+import { FoodService } from './services/food.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HeadersComponent
+    HeadersComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [FoodService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
